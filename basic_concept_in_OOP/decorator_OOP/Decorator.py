d5112f88e26924bc02bf3d901313d7ffc0a71d5f
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-08-02 09:54:27 +0200

    ;earming about decorators and shoewd the first way on how to call theses decorator

---
+++ basic_concept_in_OOP/decorator_OOP/Decorator.py
@@ -0,0 +1,15 @@
+def hello():
+    print('hello')
+    return None
+hello()
+def my_decorator(func):
+    def wrapper():
+        print('check before')
+        func()
+        print('check after')
+    return wrapper
+
+# say_hello = my_decorator(hello)
+# say_hello()
+
+
