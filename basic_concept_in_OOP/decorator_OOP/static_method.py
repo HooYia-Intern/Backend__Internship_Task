c0215ef47418d846c08482f313b2f64c829861d9
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-08-02 10:40:26 +0200

    practical example on how to apply the static method

--- basic_concept_in_OOP/decorator_OOP/static_method.py
+++ basic_concept_in_OOP/decorator_OOP/static_method.py
@@ -1,7 +1,14 @@
-def __init__(self, wheel):
-    self.wheel = wheel
-    self.Name = "class car"
-@staticmethod
-def get_class_info():
-    return "This is the child class"
-#No Argement when creating the fuction 
+class Car:
+    def __init__(self, wheel):
+        self.wheel = wheel
+        self.name = "class car"
+
+    @staticmethod
+    def get_class_info():
+        return "This is the child class"
+
+# Example usage:
+my_car = Car(4)
+print(my_car.wheel)  # Output: 4
+print(my_car.name)   # Output: class car
+print(Car.get_class_info())  # Output: This is the child class
