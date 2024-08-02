3c5cd92845707022572274e051bc2ef310b5dd52
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-08-02 10:04:23 +0200

    static method on how to use the decorator

---
+++ basic_concept_in_OOP/decorator_OOP/static_method.py
@@ -0,0 +1,7 @@
+def __init__(self, wheel):
+    self.wheel = wheel
+    self.Name = "class car"
+@staticmethod
+def get_class_info():
+    return "This is the child class"
+#No Argement when creating the fuction 
