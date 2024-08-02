11c901633c47b8754175319fdfa4d09e242d9b20
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-08-02 10:39:16 +0200

    pratical example

--- basic_concept_in_OOP/decorator_OOP/class_method.py
+++ basic_concept_in_OOP/decorator_OOP/class_method.py
@@ -1,7 +1,14 @@
-def __init__(self, wheel):
-    self.wheel = wheel
-    self.Name = "class car"
-@classmethod
-def get_info(cls):
-    return dir(cls)
-#Take an Argement when creating the fuction 
+class Car:
+    def __init__(self, wheel):
+        self.wheel = wheel
+        self.name = "class car"
+
+    @classmethod
+    def get_info(cls):
+        return dir(cls)
+
+# Example usage:
+my_car = Car(4)
+print(my_car.wheel)  # Output: 4
+print(my_car.name)   # Output: class car
+print(Car.get_info())  # Output: List of attributes and methods of the Car class
