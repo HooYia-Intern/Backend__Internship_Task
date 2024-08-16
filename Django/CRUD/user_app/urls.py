03870c55d9def96ae6b4c7a2cda8c3f0525c940b
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-08-16 11:27:16 +0200

    terminate the crud system

--- Django/CRUD/user_app/urls.py
+++ Django/CRUD/user_app/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from . import views
+from .views import home, user_update, user_delete
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('users/', views.user_list, name='user_list'),
-    path('user/<int:id>/', views.user_detail, name='user_detail'),
-    path('user/add/', views.user_create, name='user_create'),
-    path('user/<int:id>/edit/', views.user_update, name='user_update'),
-    path('user/<int:id>/delete/', views.user_delete, name='user_delete'),
+    path('', home, name='home'),
+    path('update/<int:id>/', user_update, name='user_update'),
+    path('delete/<int:id>/', user_delete, name='user_delete'),
 ]
--- Django/CRUD/user_app/views.py
+++ Django/CRUD/user_app/views.py
@@ -3,40 +3,55 @@ from .models import User
 from .forms import UserForm
 
 def home(request):
-    return render(request, 'home.html')
-
-def user_list(request):
-    users = User.objects.all()
-    return render(request, 'user_list.html', {'users': users})
-
-def user_detail(request, id):
-    user = get_object_or_404(User, id=id)
-    return render(request, 'user_detail.html', {'user': user})
-
-def user_create(request):
     if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')  # Redirect to home page after saving
-    else:
-        form = UserForm()
-    return render(request, 'user_form.html', {'form': form, 'action': 'Add'})
+        if 'create_user' in request.POST:
+            form = UserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('home')
+        elif 'update_user' in request.POST:
+            user_id = request.POST.get('user_id')
+            user = get_object_or_404(User, id=user_id)
+            form = UserForm(request.POST, instance=user)
+            if form.is_valid():
+                form.save()
+                return redirect('home')
+        elif 'delete_user' in request.POST:
+            user_id = request.POST.get('user_id')
+            user = get_object_or_404(User, id=user_id)
+            user.delete()
+            return redirect('home')
 
+    users = User.objects.all()
+    create_form = UserForm()
+
+    context = {
+        'users': users,
+        'create_form': create_form,
+    }
+
+    # Add a form for each user
+    for user in users:
+        context[f'user_update_form_{user.id}'] = UserForm(instance=user)
+
+    return render(request, 'home.html', context)
+
+# Update user view (optional if needed separately)
 def user_update(request, id):
     user = get_object_or_404(User, id=id)
     if request.method == 'POST':
         form = UserForm(request.POST, instance=user)
         if form.is_valid():
             form.save()
-            return redirect('home')  # Redirect to home page after saving
+            return redirect('home')
     else:
         form = UserForm(instance=user)
-    return render(request, 'user_form.html', {'form': form, 'action': 'Edit'})
+    return render(request, 'user_update.html', {'form': form, 'user': user})
 
+# Delete user view
 def user_delete(request, id):
     user = get_object_or_404(User, id=id)
     if request.method == 'POST':
         user.delete()
-        return redirect('home')  # Redirect to home page after deleting
+        return redirect('home')
     return render(request, 'user_confirm_delete.html', {'user': user})
